e51db4b888cb4b2a9946246f68b105c1771836be
Ben Taylor <Bentaylor721@gmail.com>
2026-01-17 15:24:36 -0500

    Added Entropy

---
+++ README.md
@@ -0,0 +1,7 @@
+Wrote a module password verification program in Python to check passwords according to length, character variety, common password lists, and entropy.
+
+Mathematically calculated entropy to estimate the unguessable nature of passwords.
+
+Produced human-readable feedback and overall strength score.
+
+Designed with the use of clean modular functions, best practices including main() entry point, tuple returns and no global state.
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-
-print('Enter a Password for Testing: ')
-password = input()
+from math import log2
 
 def fun_length(password):
+    score = 0
+    message = []
+    length = len(password)
     if length < 8:
         message.append("The Password is too short!")
     elif length <= 12:
@@ -11,9 +12,11 @@ def fun_length(password):
     else:
         message.append("The Password's length is great!")
         score += 2
-    return
+    return score, message
 
 def fun_strength(password):
+    score = 0
+    message = []
     if any(c.islower() for c in password):
         score += 1
     else:
@@ -32,19 +35,20 @@ def fun_strength(password):
     else: 
         message.append("Try Using some symbols.")
 
-    return
+    return score, message
 
 def fun_common(password):
     common_passwords = ["password","password1","password123","123456","12345678","123456789","123123","111111","qwerty","qwerty123","abc123",
     "letmein","iloveyou","admin","welcome","monkey","dragon","sunshine","football","baseball","starwars","hello","guest","login","princess",
     "master","passw0rd","trustno1","654321" ]
-
+    score = 1
+    message = []
     if password.lower() in common_passwords:
         message.append("This is a common password, Please select another one")
         score = 0
-    return
+    return score , message
 
-def fun_checker(password):
+def fun_checker(score):
     if score <= 3:
         result = "Weak Strength"
     elif score <= 5:
@@ -53,11 +57,69 @@ def fun_checker(password):
         result = "Strong"
     return result
 
-length = len(password)
-score = 0
-message = []
-print("Score:", score)
-print(fun_checker(password))
-print("Feedback: ")
-for msg in message:
-    print("-", msg)
+def entropy_size(password):
+    entropy = 0
+    if any(c.islower() for c in password):
+        entropy += 26
+    if any(c.isupper() for c in password):
+        entropy += 26
+    if any(c.isdigit() for c in password):
+        entropy += 10
+    if any(not c.isalnum() for c in password):
+        entropy += 32
+    return entropy
+
+def entropy_calculator(password):
+    length = len(password)
+    size = entropy_size(password)
+    entropy = length * log2(size)
+    return entropy
+
+def entropy_feedback(entropy):
+    message = []
+    if entropy < 28:
+        message.append("Very Weak Entropy")
+    elif entropy < 36:
+        message.append("Weak Entropy")
+    elif entropy < 60:
+        message.append("Reasonable Entropy")
+    elif entropy < 128:
+        message.append("Strong Entropy")
+    else:
+        message.append("Excessive Entropy")
+    return message
+
+
+
+
+def main():
+    print('Enter a Password for Testing: ')
+    password = input()
+    length_score, length_message = fun_length(password)
+    strength_score, strength_message = fun_strength(password)
+    common_score, common_message = fun_common(password)
+    score = length_score + strength_score
+    if common_score == 0:
+        score = 0
+    message = length_message + strength_message + common_message
+    print("Score:", score)
+    print(fun_checker(score))
+    print("Feedback: ")
+    for msg in message:
+        print("-", msg)
+    print("\n")
+
+    # Entropy
+    entropy = entropy_calculator(password)
+    entropy_message = entropy_feedback(entropy)
+    print("The Entropy is: ", entropy)
+    for msg in entropy_message:
+        print("-", msg)
+    
+
+
+    pass
+
+if __name__ == "__main__":
+    main()
+
